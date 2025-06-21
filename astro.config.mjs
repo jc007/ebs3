66f2a92da339babb239333e4b9b6bae515431815
jc007 <joh@untitleddesign.co.nz>
2025-06-21 14:57:27 +1200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://ebs3.onrender.com',
-  //base: '/ebs3', // Remove this line if using custom domain or username.github.io
   output: 'static',
   build: {
     assets: 'assets'
